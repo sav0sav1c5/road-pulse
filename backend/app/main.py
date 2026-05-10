b304ed46c4bdf3809d1444194ed834d8f1c788f5
Savo Savić <192821740+sav0sav1c5@users.noreply.github.com>
2026-05-10 19:57:57 +0200

    Initial Project

---
+++ backend/app/main.py
@@ -0,0 +1,6 @@
+import scripts.import_data as id
+
+if __name__ == "__main__":
+
+    # Load data
+    data = id.import_raw_data()
---
+++ backend/app/scripts/import_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+RAW_PATH = "data/raw/nez-opendata-202.xlsx"
+
+def add_columns(data):
+    data.columns = ['accident_id', 'department', 'municipality', 'date_time', 'longitude', 'latitude', 'accident_type', 'involved_vehicles_num', 'description']
+
+    return data
+
+def import_raw_data():
+    imported_data = []
+
+    for i in range(7):
+        data = pd.read_excel(f"../../data/raw/nez-opendata-202{i}.xlsx")
+        data = add_columns(data)
+        imported_data.append(data)
+        print(data.columns)
+
+    return pd.concat(imported_data, ignore_index=True)

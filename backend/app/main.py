431cd3919b14de306a30b0abccbf458b15ba38a0
Savo Savić <192821740+sav0sav1c5@users.noreply.github.com>
2026-05-10 23:17:18 +0200

    feat: add accident data import pipeline with SQLAlchemy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 import scripts.import_data as id
 import models.accident as acc
@@ -8,17 +9,19 @@ import models.accident as acc
 if __name__ == "__main__":
 
     # Load raw data to dataframe
-    data = id.import_raw_data()
+    # data = id.import_raw_data()
 
     # Connect with database
     load_dotenv()
-
     database_url = os.getenv("DATABASE_URL")
-
     engine = create_engine(database_url)
 
     # Creating tables
     acc.Base.metadata.create_all(engine)
 
+    # Creating sesstion for loading of data
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     # Load data in tables
-    id.load_database()
+    id.load_database(session)
--- backend/app/scripts/import_data.py
+++ backend/app/scripts/import_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from  models.accident import Accident
 
 RAW_PATH = "data/raw/nez-opendata-202.xlsx"
 
@@ -13,11 +14,42 @@ def import_raw_data():
     for i in range(7):
         data = pd.read_excel(f"../../data/raw/nez-opendata-202{i}.xlsx")
         data = add_columns(data)
+
+        # Date and Time parsing
+        data['date_time'] = pd.to_datetime(
+            data['date_time'],
+            format = "%d.%m.%Y,%H:%M",
+            errors = "coerce"
+        )
+
         imported_data.append(data)
         print(data.columns)
 
     return pd.concat(imported_data, ignore_index=True)
 
-def load_database():
+def load_database(session):
 
-    pass
+    data = import_raw_data()
+
+    data = data.drop_duplicates(subset=['accident_id'])
+
+
+    for index, row in data.iterrows():
+        
+        accident = Accident(
+            accident_id = row['accident_id'],
+            department = row['department'],
+            municipality = row['municipality'],
+            date_time = row['date_time'],
+            longitude = row['longitude'],
+            latitude = row['latitude'],
+            accident_type = row['accident_type'],
+            involved_vehicles_num = row['involved_vehicles_num'],
+            description = row['description']
+        )
+        
+        session.add(accident)
+
+    session.commit()
+
+    print("Database loaded successfully!")

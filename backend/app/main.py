00a2d9549ad8f3bdd6fd76d9825a00445d31005f
Savo Savić <192821740+sav0sav1c5@users.noreply.github.com>
2026-05-19 22:07:18 +0200

    feat: add index.html as initial frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 from backend.app.db.database import engine, Base
 from backend.app.api.accidents import router
 
@@ -8,6 +11,14 @@ app = FastAPI(title='Road Pulse API', version='1.0')
 
 # Add all endpoints from router
 app.include_router(router=router)
+
+# Using frontend/ files like static files
+app.mount('/static', StaticFiles(directory='frontend'), name='static')
+
+# Index (main) page
+@app.get('/')
+def index():
+    return FileResponse("frontend/index.html")
 
 # Health check method
 @app.get('/health')
